3dcc0ae844a4964dc108b8eaab827985776b496c
Giovanni De Toni <giovanni.det@gmail.com>
2019-05-27 17:28:36 +0200

    Add scripts to build the token/pos FSA with probabilities.

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
+build_tok_pos_counts:
+	bash utils/compute_token_pos_counts.sh
+
+build_tok_pos_prob: build_tok_pos_counts
+	python3 concept_tagger.py
+
 build_lexicon:
 	bash build_lexicon.sh
 
 clean:
-	rm lexicon.txt lexicon_base.txt lexicon_count.txt utils/converted_string.far utils/extracted.fsa utils/intersected.fsa
+	rm -f lexicon.txt lexicon_base.txt lexicon_count.txt utils/converted_string.far utils/extracted.fsa utils/intersected.fsa
+	rm -f *.prob *.counts *.txt
---
+++ utils/compute_token_pos_counts.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+# This script generates helpers files with the token
+# counts and token/pos counts given training and test data.
+#
+# Author: Giovanni De Toni
+# Date: 05/03/2019
+# Email: giovanni.det@gmail.com
+
+export train_data=""
+export test_data=""
+
+# Usage and version information
+eval "$(docopts -V - -h - : "$@" <<EOF
+Usage: ngram_pos_tagger [<train_data>] [<test_data>]
+
+Options:
+	<train_data>    Train dataset used.
+	<test_data>     Test dataset used.
+	--help			Show help options.
+	--version		Print program version.
+----
+ngram_pos_tagger 0.1.0
+Copyright (C) 2019 Giovanni De Toni
+License MIT
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+EOF
+)"
+
+# Unofficial strict bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if [ -z $train_data ]; then train_data="NL2SparQL4NLU/dataset/NL2SparQL4NLU.train.conll.txt"; fi
+if [ -z $test_data ]; then test_data="NL2SparQL4NLU/dataset/NL2SparQL4NLU.test.conll.txt"; fi
+
+# Compute POS counts
+cat $train_data | cut -f 2 |\
+sed '/ˆ *$/d' |\
+sort | uniq -c |\
+sed 's/ˆ *//g' |\
+awk '{OFS="\t"; print $2,$1}' >> POS.tmp
+echo -e "token\tcount" > POS.counts
+cat POS.tmp | tail -n +2 >> POS.counts
+
+# Compute TOK_POS counts
+cat $train_data |\
+sed '/ˆ *$/d' |\
+sort | uniq -c |\
+sed 's/ˆ *//g' |\
+awk '{OFS="\t"; print $2,$3,$1}' > TOK_POS.tmp
+
+# Dataset for the second transducer
+cat $train_data | cut -f 2 |\
+awk '!NF{$0="#"}1' |\
+tr '\n' ' ' |\
+tr '#' '\n' |\
+sed 's/^ *//g;s/ *$//g' > lex.txt
+
+cat TOK_POS.tmp | tail -n +2 > TOK_POS.counts
+
+# Remove files
+rm POS.tmp TOK_POS.tmp
+
+
+
