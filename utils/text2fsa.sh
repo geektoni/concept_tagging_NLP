0ec448da48d400a40fcc1439382192e3402abed0
Giovanni De Toni <giovanni.det@gmail.com>
2019-05-27 16:11:39 +0200

    Add a script to convert a string into an FSA.

---
+++ utils/text2fsa.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# This script is used to convert a text string
+# into an FSA, which can be later used to check
+# if a given string is recognized by another FSA.
+#
+# Author: Giovanni De Toni
+# Date: 05/03/2019
+# Email: giovanni.det@gmail.com
+
+text_string=""
+output_fsa=""
+lexicon=""
+
+# Usage and version information
+eval "$(docopts -V - -h - : "$@" <<EOF
+Usage: text2fsa <text_string> <lexicon> [<output_fsa>]
+
+Options:
+	<train_data>    Text string which will be converted to an FSA.
+	<lexicon>       Lexicon used.
+	<output_fsa>    Name of the generated fsa (default: converted_string.fsa)
+	--help			Show help options.
+	--version		Print program version.
+----
+compute_lm 0.1.0
+Copyright (C) 2019 Giovanni De Toni
+License MIT
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+EOF
+)"
+
+# Unofficial strict bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Check if a name was supplied
+if [ -z ${output_fsa} ]; then output_fsa="converted_string.fsa"; fi;
+
+# Create the fsa
+echo $output_fsa | farcompilestrings --symbols=${lexicon} -unknown_symbol='<unk>' --generate_keys=1 --keep_symbols | farextract --filename_suffix='.fst'
