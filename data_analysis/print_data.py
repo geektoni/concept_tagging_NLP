3ce257a548b28a2e01bfa41a398347ca00b8c291
Giovanni De Toni <giovanni.det@gmail.com>
2019-12-04 23:35:11 +0100

    Improve data analysis script.

--- data_analysis/print_data.py
+++ data_analysis/print_data.py
@@ -3,17 +3,36 @@ import seaborn as sns
 import pandas as pd
 sns.set()
 
-f = pd.read_csv("../NL2SparQL4NLU/dataset/NL2SparQL4NLU.train.conll.txt", delimiter="\t", header=None)[1]
+train = pd.read_csv("../NL2SparQL4NLU/dataset/NL2SparQL4NLU.train.conll.txt", delimiter="\t", header=None)[1]
+test = pd.read_csv("../NL2SparQL4NLU/dataset/NL2SparQL4NLU.test.conll.txt", delimiter="\t", header=None)[1]
 
-print(f)
+# Describe the datasets
+print(train.describe())
+print("")
+print(test.describe())
+print("")
+
+# Elements missing from the test set
+missing_elements_train = list(set(test.unique())-set(train.unique()))
+print(missing_elements_train)
+for i in missing_elements_train:
+    print(i, test[ test == i ].count(), test[ test == i ].count()/len(test))
+
+print("")
+
+# Elements missing from the train set
+missing_elements_test = list(set(train.unique())-set(test.unique()))
+print(missing_elements_test)
+for i in missing_elements_test:
+    print(i, train[ train == i ].count(), train[ train == i ].count()/len(train))
 
 plt.figure(figsize=(11,6))
 
-ax = sns.countplot(f, order=f.value_counts().index)
+ax = sns.countplot(train, order=train.value_counts().index)
 
 for p in ax.patches:
-    percentage = '{:.1f}%'.format(100 * p.get_height()/len(f))
-    if 5 > (100 * p.get_height()/len(f)):
+    percentage = '{:.1f}%'.format(100 * p.get_height()/len(train))
+    if 5 > (100 * p.get_height()/len(train)):
         continue
     x = p.get_x()
     y = p.get_y() + p.get_height()+10
@@ -26,4 +45,4 @@ plt.xticks(rotation="vertical")
 
 plt.subplots_adjust(left=.07, bottom=.32)
 plt.tight_layout()
-plt.show()
+#plt.savefig("result.png", dpi=200)
